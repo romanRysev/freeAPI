ea2de2d5e021bb0fe43e684e2675c8571b44dae3
Roman <r.rysev@tesonero.com>
2021-04-09 17:58:32 +0300

    базовая настройка проекта

---
+++ free-api/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app class="app"><Home /></v-app>
+</template>
+
+<script>
+import Home from '@/views/Home.vue'
+export default {
+  components: {
+    Home,
+  },
+}
+</script>
+
+<style></style>
---
+++ free-api/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <v-card class="pa-5 ma-5"><v-textarea /></v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
